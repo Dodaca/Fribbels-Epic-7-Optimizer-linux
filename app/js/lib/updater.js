b7736eb6d10f756b7573785fda1192b94f492564
Dominik Lampl <lampl.dominik@web.de>
2026-06-29 18:39:20 +0200

    Start patching autoupdate Return to old model and have the updater check correct Repo and telling user a new update is available

--- app/js/lib/updater.js
+++ app/js/lib/updater.js
@@ -51,91 +51,28 @@ module.exports = {
     },
 
     checkForUpdates: async () => {
-        //
-        // try {
-        //     const latestData = await fetch('https://api.github.com/repos/fribbels/Fribbels-Epic-7-Optimizer/releases/latest')
-        //     const latestDataText = await latestData.text();
-        //     const latestDataJson = JSON.parse(latestDataText);
-        //     const latestVersion = latestDataJson.tag_name;
+        
+         try {
+             const latestData = await fetch('https://api.github.com/repos/Dodaca/Fribbels-Epic-7-Optimizer-linux/releases/latest')
+             const latestDataText = await latestData.text();
+             const latestDataJson = JSON.parse(latestDataText);
+             const latestVersion = latestDataJson.tag_name;
 
-        //     if (latestVersion != currentVersion) {
-        //         const shell = require('electron').shell;
+             if (latestVersion != currentVersion) {
+                 const shell = require('electron').shell;
 
-        //         // assuming $ is jQuery
-        //         $(document).on('click', 'a[href^="http"]', function(event) {
-        //             event.preventDefault();
-        //             shell.openExternal(this.href);
-        //         });
+                 // assuming $ is jQuery
+                 $(document).on('click', 'a[href^="http"]', function(event) {
+                     event.preventDefault();
+                     shell.openExternal(this.href);
+                 });
 
-        //         Dialog.htmlSuccessDisableOutsideClick(i18next.t("New version available: <a href='https://github.com/fribbels/Fribbels-Epic-7-Optimizer/releases'>") + latestVersion + "<a>");
-        //     }
+                 Dialog.htmlSuccessDisableOutsideClick(i18next.t("New version available: <a href='https://github.com/Dodaca/Fribbels-Epic-7-Optimizer-linux/releases/tag/v1.12.1-offline-linux'>") + latestVersion + "<a>");
+             }
 
-        //     // console.error(latestDataJson);
-        // } catch (e) {
-        //     console.error(e)
-        // }
-
-        const version = document.getElementById('version');
-        version.innerText = ": v" + currentVersion;
-
-        ipcRenderer.on('update_available', () => {
-            Notifier.info(i18next.t("New version available, downloading now"));
-        });
-        ipcRenderer.on('update-not-available', () => {
-        });
-        ipcRenderer.on('test', (arg1, arg2) => {
-            console.log("test", arg1, arg2)
-        });
-        ipcRenderer.on('check', (arg1, arg2) => {
-            console.log("check", arg1, arg2)
-            try {
-                if (typeof arg2 === 'string' || arg2 instanceof String) {
-                    const response = JSON.parse(arg2);
-                    console.log(response);
-                    const updateVersion = response.updateInfo.version;
-
-                    if (currentVersion == updateVersion) {
-                        Notifier.info(i18next.t("No new updates found"));
-                    }
-                } else {
-                    const response = arg2;
-                    console.log(response);
-                    const updateVersion = response.updateInfo.version;
-
-                    if (currentVersion == updateVersion) {
-                        Notifier.info(i18next.t("No new updates found"));
-                    }
-                }
-            } catch (e) {
-                console.warn("Failed to parse IPC response", e)
-                Notifier.info(i18next.t("No new updates found"));
-            }
-        });
-        ipcRenderer.on('update_downloaded', async (arg1, arg2) => {
-            console.log("update_downloaded", arg1, arg2)
-            var response = await Dialog.updatePrompt("Update downloaded. It will be installed on restart. Restart app now?")
-
-            if (response == 'restart') {
-                await Subprocess.kill();
-                restartApp();
-            }
-        });
-
-        function restartApp() {
-            ipcRenderer.send('restart_app');
-        }
-
-
-        document.getElementById('checkForUpdatesSubmit').addEventListener("click", async () => {
-            Notifier.info(i18next.t("Checking for updates"));
-
-            try {
-                await HeroData.initialize();
-            } catch (e) {
-                console.error("Error refreshing hero data " + e)
-            }
-
-            ipcRenderer.send('check');
-        });
-    }
-}
+             // console.error(latestDataJson);
+         } catch (e) {
+             console.error(e)
+         }
+    }}
+    
